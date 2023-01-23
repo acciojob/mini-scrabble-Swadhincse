34674a7c27a594f7f57a7f801e138d63272c4cd9
Swadhin <swadhinghosh31@gmail.com>
2023-01-24 02:51:25 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+
+
+const textbox = document.getElementById("evaluatedText");
+const letterCount = document.getElementById("letterCount");
+
+
+textbox.addEventListener("input", function() {
+    
+    letterCount.innerHTML = textbox.value.length;
+});
+textbox.addEventListener("keyup", function() {
+    letterCount.innerHTML = textbox.value.length;
+});
+letterCount.innerHTML = textbox.value.length || 0;
